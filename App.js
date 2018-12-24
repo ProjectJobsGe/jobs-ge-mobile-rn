68188b34a49f104f5b52df39fde339b6b8e02bae
ODINA101 <iraklisamniashviliii@gmail.com>
2018-12-24 05:44:57 -0500

    #1 - Adding Paper to projcet.

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from "./components/main"
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#0084af',
+  }
+};
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <PaperProvider theme={theme}>
+      <Main />
+    </PaperProvider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/main/index.js
@@ -0,0 +1,39 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+import { Appbar } from 'react-native-paper';
+export default class Main extends Component {
+
+  _onSearch = () => console.log('Searching');
+
+  _onMore = () => console.log('Shown more');
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <Appbar.Header>
+
+          <Appbar.Content
+            title="Jobs.ge"
+          />
+          <Appbar.Action icon="search" onPress={this._onSearch} />
+          <Appbar.Action icon="more-vert" onPress={this._onMore} />
+        </Appbar.Header>
+
+         <Text>Content here</Text>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
